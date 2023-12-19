6c31fe2410b23cb71a39ca753284c33b495eaf02
Muaz Abdin <muathabd25@gmail.com>
2023-12-19 12:44:25 +0200

    complete the JWT spot checks

---
+++ Week6/JWT/fakeDB/users.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    id: 1,
+    username: "ameerj",
+    password: "meowmeow", // hashed password: 'password'
+    animal: "cat",
+  },
+  {
+    id: 2,
+    username: "lotemh",
+    password: "natureisamazing!", // hashed password: '123456'
+    animal: "owl",
+  },
+];
+
+const bcrypt = require("bcryptjs");
+
+//we generate the salt for the password
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+users.forEach((user) => {
+  const hashedPassword = bcrypt.hashSync(user.password, salt);
+  user.password = hashedPassword;
+});
+
+module.exports = users;
